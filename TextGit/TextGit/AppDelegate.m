822d07d98b18486cecdf7b5c018f63c6b1fd1f3b
sunbo91611 <757854323@qq.com>
2019-08-20 19:14:26 +0800

    更改代码

--- TextGit/TextGit/AppDelegate.m
+++ TextGit/TextGit/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"更改代码");
     return YES;
 }
 
